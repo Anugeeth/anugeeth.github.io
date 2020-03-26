034cfe9c28e744588dbfa8c6ad9fe9ea6efb3fc4
anugeeth <anugeeth2@gmail.com>
2020-03-26 09:34:14 +0530

    nothing new 😑

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-<div>
-    <p class="text-center line-1 anim-typewriter">Not yet ready !!!!</p> </div>
+  <div>
+    <p class="line-1 anim-typewriter">Not yet ready !!!!</p>
+    <div
+      class="wrapper"
+    >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis libero voluptates quam dolores illo laudantium vel, veritatis illum officia exercitationem harum sequi tenetur iusto minus voluptate numquam corrupti voluptatibus fuga. Fugiat deserunt laborum, molestiae rerum incidunt atque. Animi aspernatur, inventore error autem atque assumenda exercitationem, nihil laborum quibusdam optio voluptates fugit deserunt debitis labore repudiandae ad necessitatibus fuga dicta, eos quae rem? Repellendus temporibus atque magni. Nostrum omnis consequuntur iusto qui error odio ducimus, ad reiciendis, deleniti sunt fugiat aliquam suscipit quam debitis tempora quae adipisci quas numquam possimus? At, impedit dolore ex officia ab cupiditate facilis? Aliquam cum, explicabo consequuntur recusandae veniam minima aliquid doloremque voluptas quidem possimus facere voluptatem officiis qui ullam unde illum nemo ea quibusdam molestiae accusantium repellat tenetur, non corporis! Debitis voluptate eius alias animi expedita atque porro odit tempora voluptatum accusamus! Sapiente, explicabo labore quaerat sit, eum quae, incidunt similique quas laboriosam soluta tenetur impedit obcaecati natus repudiandae ab ratione modi possimus odit alias quos! Praesentium debitis distinctio libero tenetur iusto perferendis impedit neque. Officiis recusandae ab magni, aliquam cumque labore dolorem harum deserunt numquam quod! Qui pariatur eaque, omnis exercitationem iusto quidem ex molestias ipsum, unde sequi maiores asperiores quasi ab quia laborum!</div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +14,26 @@ export default {
 </script>
 
 <style>
+
+
+.wrapper {
+
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-auto-rows: auto;
+
+
+
+
+   background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 5px;
+  box-shadow: 0px 5px 10px rgba(255, 255, 255, 0.75);
+  color: #000000;
+  padding:20px;
+  margin: 10px;
+}
+
+
 .line-1 {
   position: relative;
   top: 50%;
